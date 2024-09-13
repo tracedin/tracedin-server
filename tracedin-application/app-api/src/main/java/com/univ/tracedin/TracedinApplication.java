e89fd3c38921d0855dc360bd4305283845695f45
강지원 <kangji149@naver.com>
2024-09-13 18:18:54 +0900

    init : first init

---
+++ tracedin-application/app-api/src/main/java/com/univ/tracedin/TracedinApplication.java
@@ -0,0 +1,12 @@
+package com.univ.tracedin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracedinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TracedinApplication.class, args);
+    }
+}
---
+++ tracedin-application/app-api/src/test/java/com/univ/tracedin/TracedinApplicationTests.java
@@ -0,0 +1,11 @@
+package com.univ.tracedin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracedinApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
