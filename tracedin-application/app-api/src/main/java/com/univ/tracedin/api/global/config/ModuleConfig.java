dbfd9bfe06b601d1d53b81d0a4235e250c8297d7
강지원 <kangji149@naver.com>
2024-10-20 22:23:57 +0900

    feat : change java time module format

--- tracedin-application/app-api/src/main/java/com/univ/tracedin/api/global/config/ModuleConfig.java
+++ tracedin-application/app-api/src/main/java/com/univ/tracedin/api/global/config/ModuleConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
@@ -13,6 +14,7 @@ public class ModuleConfig {
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return objectMapper;
     }
 }
