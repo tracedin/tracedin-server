88b6d4ff504b614bc0b90b249ab22549ac9c339c
강지원 <kangji149@naver.com>
2024-10-21 16:19:15 +0900

    docs : 메트릭 SSE API 설명

--- tracedin-application/app-api/src/main/java/com/univ/tracedin/api/metric/ServiceMetricsApiDocs.java
+++ tracedin-application/app-api/src/main/java/com/univ/tracedin/api/metric/ServiceMetricsApiDocs.java
@@ -2,6 +2,9 @@ package com.univ.tracedin.api.metric;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
 import com.univ.tracedin.api.global.dto.Response;
 import com.univ.tracedin.api.metric.dto.AppendServiceMetricsRequest;
 import com.univ.tracedin.api.metric.dto.HttpRequestCountResponse;
@@ -12,6 +15,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "메트릭 API")
 public interface ServiceMetricsApiDocs {
+
+    @Operation(summary = "메트릭 SSE 구독", description = "SSE 이벤트를 구독하여 메트릭을 실시간으로 받을 수 있습니다.")
+    ResponseEntity<SseEmitter> subscribe(String projectKey, String serviceName);
 
     @Operation(summary = "메트릭 추가(라이브러리 전용)")
     void appendMetrics(AppendServiceMetricsRequest requests);
